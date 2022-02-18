9368c23c1cf395ba9efd72c82dd1ae7f7daa8980
fjaviersaezm <fjaviersaezm@gmail.com>
2022-02-18 13:32:24 +0100

    Exercise 3.c

--- PL/pl.py
+++ PL/pl.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 # z = 2x1 + 5x2
 # x1 + x2 >= 4
@@ -21,17 +22,21 @@ y1 = 4 - x1
 
 # Plot restrictions
 
+figure(figsize=(20,15))
+
 plt.axvline(0.0, color = "black",label = "$x_1,x_2 \geq 0$")
 plt.axhline(0.0, color = "black")
-plt.plot(x,y1,color = "red",label = "$x_1 + x_2 \geq 4$")
-plt.axvline(2.0, color = "blue",label = "$x_1 \geq 2")
+plt.plot(x1,y1,color = "tomato",label = "$x_1 + x_2 \geq 4$")
+plt.axvline(2.0, color = "teal",label = "$x_1 \geq 2$")
 
 # Feasible region
 plt.fill_between(x1,
-                np.maximum(25 - (1.5*x_1), (2*(x_1))),
-                np.minimum(25 - (1.5*x_1), 8), 
+                np.minimum(4-x1,2*np.ones(len(x1))),
+                10,
                 where=x1 >= 2,
-color='green', alpha=0.25)
+                color='cornflowerblue',
+                 alpha=0.25,
+                 label = "Feasible region")
 
 
 
@@ -41,6 +46,6 @@ plt.ylabel("$x_2$")
 plt.legend()
 plt.axis("on")
 
-plt.xlim(0,5)
+plt.xlim(1.5,5)
 plt.ylim(0,5)
 plt.show()
